d8752dd55c55e7fdc86c8b7ebf2110284d97d4f2
CandyyPL <aplay58.yt@gmail.com>
2026-02-16 17:25:50 +0100

    refactor keyboard component

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,41 +1,40 @@
 import style from "./Keyboard.module.css";
 
+type KeyData = {
+  style: string;
+  value: string;
+};
+
 const Keyboard = () => {
-  const rows = [
-    Array.from("qwertyuiop"),
-    Array.from("asdfghjkl"),
-    Array.from("@zxcvbnm#"),
-  ];
+  const rows = ["qwertyuiop", "asdfghjkl", "@zxcvbnm#"];
+
+  const getKeyData = (char: string): KeyData => {
+    const baseStyle = [style.keyboardKey];
+
+    if (char === "@")
+      return {
+        style: baseStyle.concat([style.wideKey, style.enterKey]).join(" "),
+        value: "↵",
+      };
+    if (char === "#")
+      return { style: baseStyle.concat([style.wideKey]).join(" "), value: "⌫" };
+
+    return { style: baseStyle.join(" "), value: char.toUpperCase() };
+  };
 
   return (
     <section className={style.keyboardWrapper}>
-      {rows.map((row, rId) => (
-        <div key={rId} className={style.keyboardRow}>
-          {row.map((letter, lId) => (
-            <>
-              {letter === "@" && (
-                <div
-                  key={`${rId}${lId}`}
-                  className={`${style.keyboardKey} ${style.wideKey} ${style.enterKey}`}
-                >
-                  ↵
-                </div>
-              )}
-              {letter === "#" && (
-                <div
-                  key={`${rId}${lId}`}
-                  className={`${style.keyboardKey} ${style.wideKey}`}
-                >
-                  ⌫
-                </div>
-              )}
-              {letter !== "@" && letter !== "#" && (
-                <div key={`${rId}${lId}`} className={style.keyboardKey}>
-                  {letter.toUpperCase()}
-                </div>
-              )}
-            </>
-          ))}
+      {rows.map((row) => (
+        <div key={row} className={style.keyboardRow}>
+          {Array.from(row).map((letter) => {
+            const { style, value } = getKeyData(letter);
+
+            return (
+              <div key={letter}>
+                <span className={style}>{value}</span>
+              </div>
+            );
+          })}
         </div>
       ))}
     </section>
