b9c07f97d37305610ddd35bccafb83a0c0ddf2e7
CandyyPL <aplay58.yt@gmail.com>
2026-02-17 15:28:24 +0100

    add keyboard component logic

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,11 +1,15 @@
+import useGame from "@/hooks/useGame";
 import style from "./Keyboard.module.css";
 
 type KeyData = {
   style: string;
   value: string;
+  key: string;
 };
 
 const Keyboard = () => {
+  const { handleKeyPress } = useGame();
+
   const rows = ["qwertyuiop", "asdfghjkl", "@zxcvbnm#"];
 
   const getKeyData = (char: string): KeyData => {
@@ -15,11 +19,20 @@ const Keyboard = () => {
       return {
         style: baseStyle.concat([style.wideKey, style.enterKey]).join(" "),
         value: "↵",
+        key: "Enter",
       };
     if (char === "#")
-      return { style: baseStyle.concat([style.wideKey]).join(" "), value: "⌫" };
+      return {
+        style: baseStyle.concat([style.wideKey]).join(" "),
+        value: "⌫",
+        key: "Backspace",
+      };
 
-    return { style: baseStyle.join(" "), value: char.toUpperCase() };
+    return {
+      style: baseStyle.join(" "),
+      value: char.toUpperCase(),
+      key: char,
+    };
   };
 
   return (
@@ -27,12 +40,16 @@ const Keyboard = () => {
       {rows.map((row) => (
         <div key={row} className={style.keyboardRow}>
           {Array.from(row).map((letter) => {
-            const { style, value } = getKeyData(letter);
+            const { style, value, key } = getKeyData(letter);
 
             return (
-              <div key={letter}>
-                <span className={style}>{value}</span>
-              </div>
+              <button
+                key={letter}
+                className={style}
+                onClick={() => handleKeyPress(key)}
+              >
+                <span>{value}</span>
+              </button>
             );
           })}
         </div>
